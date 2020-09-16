aac1ced5dd07532e8572831a9acb977f18903039
JAMES MASON <jmason888@gmail.com>
2020-09-16 01:05:26 -0700

    Add Parameter 12 (Association Behavior) to getParameterNumbers() on dimmers.  All the other code for this parameter appears to be present already.

--- devicetypes/inovelliusa/inovelli-dimmer-lzw31.src/inovelli-dimmer-lzw31.groovy
+++ devicetypes/inovelliusa/inovelli-dimmer-lzw31.src/inovelli-dimmer-lzw31.groovy
@@ -435,7 +435,7 @@ def getParameter(number) {
 }
 
 def getParameterNumbers(){
-    return [1,2,3,4,5,6,7,8,9,10,11,13,14,15,17,21,22,51,52]
+    return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,21,22,51,52]
 }
 
 def getParameterInfo(number, value){
--- devicetypes/inovelliusa/inovelli-dimmer-red-series-lzw31-sn.src/inovelli-dimmer-red-series-lzw31-sn.groovy
+++ devicetypes/inovelliusa/inovelli-dimmer-red-series-lzw31-sn.src/inovelli-dimmer-red-series-lzw31-sn.groovy
@@ -884,7 +884,7 @@ def getParameter(number) {
 }
 
 def getParameterNumbers(){
-    return [1,2,3,4,5,6,7,8,9,10,11,13,14,15,17,18,19,20,21,22,51,52]
+    return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,51,52]
 }
 
 def getParameterInfo(number, value){
